3b4771b5aa288de3aa565ffbf518b9682d1d1f72
Geraldo Luiz <dev.geraldoluiz@gmail.com>
2020-03-21 23:58:39 -0300

    adicionando _05_operacoes_matematicas

---
+++ src/estrutura_sequencial_exercicios/_05_Operacoes_matematicas.java
@@ -0,0 +1,17 @@
+package estrutura_sequencial_exercicios;
+import java.util.Scanner;
+
+public class _05_Operacoes_matematicas {
+	
+	public static void main(String[] args) {
+	Scanner leia = new Scanner (System.in);
+		
+		
+		System.out.println("ola mundo");
+		
+		
+		
+	leia.close();
+	}
+
+}
